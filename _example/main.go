6814607b1fc68e5b89c528aa78e816773c9b4bc6
Jason Song <i@wolfogre.com>
2019-12-15 16:45:40 +0800

    docs: add example

--- _example/main.go
+++ _example/main.go
@@ -1,13 +1,3 @@
-# uniq
-
-[![Go Action](https://github.com/gochore/uniq/workflows/Go/badge.svg)](https://github.com/gochore/uniq/actions)
-[![Go Report Card](https://goreportcard.com/badge/github.com/gochore/uniq)](https://goreportcard.com/report/github.com/gochore/uniq)
-[![GitHub go.mod Go version](https://img.shields.io/github/go-mod/go-version/gochore/uniq)](https://github.com/gochore/uniq/blob/master/go.mod)
-[![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/gochore/uniq)](https://github.com/gochore/uniq/releases)
-
-Sort and deduplicate data.
-
-```go
 package main
 
 import (
@@ -42,4 +32,3 @@ type KV struct {
 	K string
 	V int
 }
-```
