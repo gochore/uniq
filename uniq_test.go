7d9cd653c232ff9f8c6eea27d03517df30250b00
Jason Song <i@wolfogre.com>
2019-12-16 15:54:08 +0800

    test: stable sorting

--- uniq_test.go
+++ uniq_test.go
@@ -58,7 +58,116 @@ func TestSort(t *testing.T) {
 	}
 }
 
+func TestStable(t *testing.T) {
+	type args struct {
+		data sort.Interface
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "emtpy",
+			args: args{
+				data: sort.IntSlice{},
+			},
+		},
+		{
+			name: "IntSlice",
+			args: args{
+				data: sort.IntSlice{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3},
+			},
+		},
+		{
+			name: "Float64Slice",
+			args: args{
+				data: sort.Float64Slice{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3},
+			},
+		},
+		{
+			name: "StringSlice",
+			args: args{
+				data: sort.StringSlice{"1", "9", "9", "4", "0", "9", "2", "6", "2", "0", "1", "4", "0", "7", "1", "3"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log(tt.args.data)
+			defer func() {
+				t.Log(tt.args.data)
+			}()
+			uniq.Sort(tt.args.data)
+			rv := reflect.ValueOf(tt.args.data)
+			tt.args.data = rv.Slice(0, uniq.Sort(tt.args.data)).Interface().(sort.Interface)
+			if !uniq.IsSorted(tt.args.data) {
+				t.Fail()
+				t.Log(tt.args.data)
+			}
+		})
+	}
+}
+
 func TestSlice(t *testing.T) {
+	intSlice := sort.IntSlice{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3}
+	ints := []int{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3}
+
+	type args struct {
+		slice interface{}
+		less  func(i, j int) bool
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "empty IntSlice",
+			args: args{
+				slice: sort.IntSlice{},
+				less:  nil,
+			},
+		},
+		{
+			name: "IntSlice",
+			args: args{
+				slice: intSlice,
+				less:  intSlice.Less,
+			},
+		},
+		{
+			name: "[]int",
+			args: args{
+				slice: ints,
+				less: func(i, j int) bool {
+					return ints[i] < ints[j]
+				},
+			},
+		},
+		{
+			name: "empty []int",
+			args: args{
+				slice: []int{},
+				less:  nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log(tt.args.slice)
+			defer func() {
+				t.Log(tt.args.slice)
+			}()
+			rv := reflect.ValueOf(tt.args.slice)
+			tt.args.slice = rv.Slice(0, uniq.Slice(tt.args.slice, tt.args.less)).Interface()
+			if !uniq.SliceIsSorted(tt.args.slice, tt.args.less) {
+				t.Fail()
+				t.Log(tt.args.slice)
+			}
+		})
+	}
+}
+
+func TestSliceStable(t *testing.T) {
 	intSlice := sort.IntSlice{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3}
 	ints := []int{1, 9, 9, 4, 0, 9, 2, 6, 2, 0, 1, 4, 0, 7, 1, 3}
 
